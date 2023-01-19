9a43b85cebf9a8d03a166297b0f360e5939052b2
nayara <a2208030@institutmontilivi.cat>
2023-01-19 15:47:09 +0100

    CAMBIOS NAYARA

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,10 @@
 ﻿internal class Program
 {
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, Montlivi");
         Console.WriteLine("Tu quieres ney");
+        Console.WriteLine("HOLA MAMADOU GUAPETON SOY NAYARA");
     }
 }
